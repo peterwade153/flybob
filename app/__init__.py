cf2d7a21c6967a2f359e7701c8f720a98cc3ed6f
peterwade153 <peterwade153@gmail.com>
2019-03-22 19:21:00 +0300

    setup environment configurations

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,18 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
+from .config import app_configuration
+
 app = Flask(__name__)
 
 #load dotenv in the base root
 app_root = os.path.join(os.path.dirname(__file__), '..')
-dotenv_path = os.path.join('app_root', '.env')
+dotenv_path = os.path.join(app_root, '.env')
+
 load_dotenv(dotenv_path)
 
+app_environment = os.getenv('APP_SETTINGS')
+app.config.from_object(app_configuration[app_environment])
 
 
 @app.route('/')
---
+++ app/config.py
@@ -0,0 +1,36 @@
+
+import os
+
+from dotenv import load_dotenv
+
+app_root = os.path.join(os.path.dirname(__file__), '..')
+dotenv_path = os.path.join(app_root, '.env')
+
+
+class Config(object):
+	DEBUG = False
+	TESTING = False
+	SECRET_KEY = os.getenv('SECRET_KEY')
+	SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+class ProductionConfig(Config):
+	DEBUG = False
+
+class StagingConfig(Config):
+	DEVELOPMENT = True
+	DEBUG = True
+
+class DevelopmentConfig(Config):
+	DEVELOPMENT = True
+	DEBUG = True
+
+class TestingConfig(Config):
+	TESTING = True
+	DEBUG=True
+	SQLALCHEMY_DATABASE_URI = "postgresql://@localhost/testflybob"
+
+app_configuration = {
+    'production': ProductionConfig,
+    'development': DevelopmentConfig,
+    'testing': TestingConfig
+}
