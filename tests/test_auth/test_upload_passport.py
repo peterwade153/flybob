f58a471bf7e5563d5a7e08bf3ab30296dcea2828
peterwade153 <peterwade153@gmail.com>
2020-02-18 11:02:49 +0300

    refactor tests

--- tests/test_auth/test_upload_passport.py
+++ tests/test_auth/test_upload_passport.py
@@ -1,23 +1,24 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 import io
 
-from app import app
 from app.auth.views import UserPassportphotoView
 from app.auth import views
 
 
 class AuthUploadPassportPhotoTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        app.testing = True
 
     @patch.object(views.UserPassportphotoView, "post")
     def test_upload_passport_photo(self, mock_post):
-        oth = UserPassportphotoView()
-        oth.post(
+        upload = UserPassportphotoView()
+        mock_post.return_value.status_code = 200
+        res = upload.post(
             "/api/v1/upload",
             data=dict(file=(io.BytesIO(b"abcdef"), "test.jpg")),
             headers={"Content-Type": "multipart/form-data"},
         )
-        self.assertEqual(mock_post.called, True)
+        self.assertEqual(res.status_code, 200)
+
+    @patch.object(views.UserPassportphotoView, "post")
+    def test_upload_photo_with_non_allowed_ext(self, mock_post):
+        mock_post.return_value = Mock(status_code=400, json=lambda : {"message": "No image was uploaded", "status": "Failed"})
