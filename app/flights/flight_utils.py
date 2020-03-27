85fcd401bffed9adb06e7943f0c748be822fac75
peter walugembe <peterwade153@gmail.com>
2020-03-27 13:11:33 +0300

    Re clean up (#32)
    * clean up commit
    
    * clean up tests
    
    * clean up flights
    
    * clean up
    
    * update
    
    * clean up update

--- app/flights/flight_utils.py
+++ app/flights/flight_utils.py
@@ -1,5 +1,7 @@
 from flask import jsonify
 
+fields = ["name","origin","destination","departure","arrive","duration","status","aircraft","capacity"]
+
 def check_all_fields_flight_register(data):
     """
     Returns an error if a required field is missing
@@ -39,3 +41,22 @@ def check_all_fields_flight_register(data):
             ),
             400,
         )
+
+def check_flight_fields_update(data):
+    """
+    Return as error if an incorrect fields passed in flight data
+    params: data
+    """
+    
+    for field in data.keys():
+        if field not in fields:
+            return (
+                jsonify(
+                    {
+                        "message": " Unknown fields passed",
+                        "status": "Failed",
+                    }
+                ),
+                400,
+            )
+
